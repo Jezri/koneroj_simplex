19a55ee358157985ea3450b26b14e8e6101225d1
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-04-28 16:43:13 +0200

    move_solution

--- move_solution_to_positive_sector.py
+++ move_solution_to_positive_sector.py
@@ -1,3 +1,7 @@
 from koneroj import *
-move_solution_to_positive_sector = writable_technique("Move solution to positivesector")
-
+from replace_unbounded_veriable import*
+move_solution_to_positive_sector = writable_technique("Move solution to positive sector")
+move_solution_to_positive_sector.parts["Procedure"].desplay+=r"""$$"""
+move_solution_to_positive_sector.parts["Procedure"].add_technique(replace_unbounded_veriable,r"""\text{ Replace all unbounded or negative veriable with positive ones}""")
+move_solution_to_positive_sector.parts["Procedure"].desplay +=r"""$$"""
+move_solution_to_positive_sector.write_module_doc()
