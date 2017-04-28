3bc04bc54bc73ba255b942bf97801cbef792c9c5
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-04-28 16:22:56 +0200

    unboubed veriable

--- replace_unbounded_veriable.py
+++ replace_unbounded_veriable.py
@@ -1,3 +1,5 @@
 from koneroj import *
 
 replace_unbounded_veriable = writable_technique("Replace unbounded veriable")
+replace_unbounded_veriable.parts["Procedure"].desplay += r""" If we have a veriable $x$ which is not definitly positive we can replace with two deinifitly positive veriables $$x \prime -x \prime \prime \quad \text{where}\quad x \prime, x \prime \prime \geq 0 $$ """ 
+replace_unbounded_veriable.write_module_doc()
